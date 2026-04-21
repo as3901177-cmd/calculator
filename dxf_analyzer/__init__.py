1ab73d90d80f6d14ffe59a3c9343cdb0f032299f
as3901177-cmd <as3901177@gmail.com>
2026-04-21 21:24:32 +0300

    Delete dxf_analyzer directory

--- dxf_analyzer/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Модульная система для анализа DXF чертежей.
-"""
-
-from .config import (
-    MAX_SPLINE_POINTS, SPLINE_FLATTENING, MAX_LENGTH, MIN_LENGTH,
-    BULGE_EPSILON, COORD_EPSILON, PIERCING_TOLERANCE, MAX_FILE_SIZE_MB,
-    get_aci_color, get_color_name
-)
